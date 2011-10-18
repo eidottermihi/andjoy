525e5de3270e8717d9e31c18165fb33ade93eda4
eidottermihi@gmail.com <eidottermihi@gmail.com@545d5380-2954-dfb3-e04e-68d3bcebc998>
2011-10-18 07:27:14 +0000

    Prototyp für "Video von Webserver abspielen" implementiert.

---
+++ trunk/andjoy/src/de/fhhof/andjoy/VideoWebserverActivity.java
@@ -0,0 +1,21 @@
+package de.fhhof.andjoy;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoWebserverActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String src = "http://h1930837.stratoserver.net:8080/LanCenter/video/testvideo.3gp";
+        setContentView(R.layout.main);
+        VideoView myView = (VideoView) findViewById(R.id.videoView1);
+        myView.setVideoURI(Uri.parse(src));
+        myView.setMediaController(new MediaController(this));
+        myView.start();
+    }
+}
