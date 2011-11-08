f57073fc99a41ee73be4d3fccd3fd599e8fa4394
eidottermihi@gmail.com <eidottermihi@gmail.com@545d5380-2954-dfb3-e04e-68d3bcebc998>
2011-11-08 08:17:14 +0000

    Experimental!"

--- trunk/andjoy/src/de/fhhof/andjoy/Menu.java
+++ trunk/andjoy/src/de/fhhof/andjoy/Menu.java
@@ -2,11 +2,16 @@ package de.fhhof.andjoy;
 
 import java.io.IOException;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
-import org.jdom.xpath.XPath;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -20,14 +25,14 @@ public class Menu extends Activity implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
-		//1: To add a new Button first start in the menu.xml
+		// 1: To add a new Button first start in the menu.xml
 		initiateClickListener();
-		
+
 	}
 
-	/**2:
-	 * This method sets all clickListeners for the Buttons on the Main menu.
-	 * To initiate a new Button copy a line and change the imageButtonXX to the 
+	/**
+	 * 2: This method sets all clickListeners for the Buttons on the Main menu.
+	 * To initiate a new Button copy a line and change the imageButtonXX to the
 	 * value u chose in the menu.xml for ur new Button.
 	 */
 	private void initiateClickListener() {
@@ -41,75 +46,92 @@ public class Menu extends Activity implements OnClickListener {
 		findViewById(R.id.imageButton24).setOnClickListener(this);
 	}
 
-	/**3:
-	 * This method represents the clickListener for the Button of the Main menu
-	 * The Listener links the Buttons created in the menu.xml with a new Activity.
-	 * To add a new Listener copy a case-Statement and change "R.id.imageButtonXX" to the 
-	 * value of the Button u initiated in the menu.xml and defined in the initiateClickListener() method.
+	/**
+	 * 3: This method represents the clickListener for the Button of the Main
+	 * menu The Listener links the Buttons created in the menu.xml with a new
+	 * Activity. To add a new Listener copy a case-Statement and change
+	 * "R.id.imageButtonXX" to the value of the Button u initiated in the
+	 * menu.xml and defined in the initiateClickListener() method.
 	 */
 	public void onClick(View v) {
 		int viewId = v.getId();
-		MediaInfo m;
+		MediaInfo m = null;
+
 		try {
 			m = getMediaInfo("imageButton11");
-		
-		Intent intent = new Intent(this, VideoWebserverActivity.class);
-		intent.putExtra("test", m);
-
-		switch (viewId) {
-		case R.id.imageButton11:
-			startActivity(intent);
-			
-			break;
-		case R.id.imageButton12:
-			startActivity(intent);
-			break;
-		case R.id.imageButton13:
-			startActivity(intent);
-			break;
-		case R.id.imageButton14:
-			startActivity(intent);
-			break;
-		case R.id.imageButton21:
-			startActivity(intent);
-			break;
-		case R.id.imageButton22:
-			startActivity(intent);
-			break;
-		case R.id.imageButton23:
-			startActivity(intent);
-			break;
-		case R.id.imageButton24:
-			startActivity(intent);
-			break;
-
-		default:
-			break;
-		}
-		} catch (JDOMException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		}
+		if(m != null){
+			Intent intent = new Intent(this, VideoWebserverActivity.class);
+			intent.putExtra("test", m);
+			
+			switch (viewId) {
+			case R.id.imageButton11:
+				startActivity(intent);
+				
+				break;
+			case R.id.imageButton12:
+				startActivity(intent);
+				break;
+			case R.id.imageButton13:
+				startActivity(intent);
+				break;
+			case R.id.imageButton14:
+				startActivity(intent);
+				break;
+			case R.id.imageButton21:
+				startActivity(intent);
+				break;
+			case R.id.imageButton22:
+				startActivity(intent);
+				break;
+			case R.id.imageButton23:
+				startActivity(intent);
+				break;
+			case R.id.imageButton24:
+				startActivity(intent);
+				break;
+				
+			default:
+				break;
+			}
+			
+		}
+
 	}
-	
-	private MediaInfo getMediaInfo(String ButtonId) throws JDOMException, IOException{
-		
+
+	private MediaInfo getMediaInfo(String ButtonId) throws IOException,
+			SAXException, ParserConfigurationException {
+
 		MediaInfo mediaInfo = new MediaInfo();
-		Document doc = new SAXBuilder().build("../XML/mediainfo.xml");
-		Element videoElement = (Element) XPath.selectSingleNode(doc, "/mediaInfo/media/video-url");
-		String videoUrl = videoElement.getText();
-		String audioUrl;
-		String imageUrl;
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(new InputSource("XML/mediainfo.xml"));
+		doc.getDocumentElement().normalize();
+
+		NodeList nodeList = doc.getElementsByTagName("media");
+		String videoUrl = null;
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node node = nodeList.item(i);
+			Element e = (Element) node;
+			if (e.getAttribute("button").equals(ButtonId)) {
+				videoUrl = e.getFirstChild().getNodeValue();
+
+			}
+
+		}
 		String textUrl = "I m text URL";
-		String headLine= "I m headline";
+		String headLine = "I m headline";
 		mediaInfo.setVideoUrl(videoUrl);
 		mediaInfo.setTextUrl(textUrl);
 		mediaInfo.setHeadLine(headLine);
 		return mediaInfo;
-		
+
 	}
 
 }
